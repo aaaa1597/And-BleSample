66aa6da3acf6cfd8b38a13abc5ad2ca38ad6bed8
aaaaa <aaaaa>
2021-11-27 12:05:15 +0900

    リファクタリング

--- Peripheral/app/src/main/java/com/test/blesample/peripheral/MainActivity.java
+++ Peripheral/app/src/main/java/com/test/blesample/peripheral/MainActivity.java
@@ -38,20 +38,29 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		/* Centralへ送信ボタン */
+		/* 通知ボタン(Centralへ送信) */
 		findViewById(R.id.button_notify).setOnClickListener(view -> {
-			notifyCharacteristicChanged();
+			boolean indicate = (mSampleCharacteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_INDICATE) == BluetoothGattCharacteristic.PROPERTY_INDICATE;
+			for (BluetoothDevice device : mBluetoothDevices) {
+				mGattServer.notifyCharacteristicChanged(device, mSampleCharacteristic, indicate);
+			}
 		});
+
 		/* アドバタイズ開始ボタン */
 		findViewById(R.id.advertise_switch).setOnClickListener(view -> {
 			if (((Switch)view).isChecked())
-				startAdvertising();
-			else
-				stopAdvertising();
+				/* アドバタイズ開始(サービス起動) */
+				startService(new Intent(getApplicationContext(), PeripheralAdvertiseService.class));
+			else {
+				/* アドバタイズ停止(サービス終了) */
+				stopService(new Intent(getApplicationContext(), PeripheralAdvertiseService.class));
+			}
 		});
+
 		/* 値変更ボタン */
 		((RadioGroup)findViewById(R.id.color_switch)).setOnCheckedChangeListener((group, checkedId) -> {
-			setCharacteristic(checkedId);
+			int value = checkedId == R.id.rbn_value_1 ? BLEMSG_1 : BLEMSG_2;
+			mSampleCharacteristic.setValue(int2bytes(value));
 		});
 
 		/* Bluetoothのサポート状況チェック 未サポート端末なら起動しない */
@@ -78,8 +87,12 @@ public class MainActivity extends AppCompatActivity {
 		else {
 			/* Bluetooth機能ONだった */
 			mGattServer = bluetoothManager.openGattServer(this, mGattServerCallback);
+			if(mGattServer == null) {
+				MsgPopUp.create(MainActivity.this).setErrMsg("Ble初期化に失敗!!\n終了します。再起動で直る可能性があります。").Show(MainActivity.this);
+				finish();
+			}
 			/* 自分自身のペリフェラル特性を定義 */
-			setBluetoothService();
+			defineOwnCharacteristic();
 		}
 	}
 
@@ -90,105 +103,33 @@ public class MainActivity extends AppCompatActivity {
 			/* Bluetooth機能ONになった。 */
 			BluetoothManager bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
 			mGattServer = bluetoothManager.openGattServer(getApplicationContext(), mGattServerCallback);
+			if(mGattServer == null) {
+				MsgPopUp.create(MainActivity.this).setErrMsg("Ble初期化に失敗!!\n終了します。再起動で直る可能性があります。").Show(MainActivity.this);
+				finish();
+			}
 			/* 自分自身のペリフェラル特性を定義 */
-			setBluetoothService();
+			defineOwnCharacteristic();
 		}
 		else {
 			MsgPopUp.create(MainActivity.this).setErrMsg("Bluetooth機能をONにする必要があります。").Show(MainActivity.this);
 		}
 	}
 
-	private void setBluetoothService() {
+	/* 自身のペリフェラル特性を定義 */
+	private void defineOwnCharacteristic() {
 		/* 自身が提供するサービスを定義 */
-		BluetoothGattService sampleService = new BluetoothGattService(UWS_SERVICE_UUID, BluetoothGattService.SERVICE_TYPE_PRIMARY);
+		BluetoothGattService ownService = new BluetoothGattService(UWS_SERVICE_UUID, BluetoothGattService.SERVICE_TYPE_PRIMARY);
 
-		/*
-		create the Characteristic.
-		we need to grant to the Client permission to read (for when the user clicks the "Request Characteristic" button).
-		no need for notify permission as this is an action the Server initiate.
-		 */
-		/* 自身が提供するCharacteristic(特性)を定義 :  */
+		/* 自身が提供するCharacteristic(特性)を定義 : 通知と読込みに対し、読込み許可 */
 		mSampleCharacteristic = new BluetoothGattCharacteristic(UWS_CHARACTERISTIC_SAMLE_UUID, BluetoothGattCharacteristic.PROPERTY_NOTIFY | BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ);
-		setCharacteristic(); // set initial state
+		mSampleCharacteristic.setValue(int2bytes(BLEMSG_1));
 
-		// add the Characteristic to the Service
-		sampleService.addCharacteristic(mSampleCharacteristic);
+		/* 定義したサービスにCharacteristic(特性)を付与 */
+		ownService.addCharacteristic(mSampleCharacteristic);
 
-		// add the Service to the Server/Peripheral
-		if (mGattServer != null) {
-			mGattServer.addService(sampleService);
-		}
+		/* 定義したサービスを有効化 */
+		mGattServer.addService(ownService);
 	}
-
-	/**
-	 * Starts BLE Advertising by starting {@code PeripheralAdvertiseService}.
-	 */
-	private void startAdvertising() {
-		// TODO bluetooth - maybe bindService? what happens when closing app?
-		startService(getServiceIntent(getApplicationContext()));
-	}
-
-
-	/**
-	 * Stops BLE Advertising by stopping {@code PeripheralAdvertiseService}.
-	 */
-	private void stopAdvertising() {
-		stopService(getServiceIntent(getApplicationContext()));
-		((Switch)findViewById(R.id.advertise_switch)).setChecked(false);
-	}
-
-
-
-	private void setCharacteristic() {
-		setCharacteristic(R.id.rbn_value_1);
-	}
-
-	/*
-	update the value of Characteristic.
-	the client will receive the Characteristic value when:
-		1. the Client user clicks the "Request Characteristic" button
-		2. teh Server user clicks the "Notify Client" button
-
-	value - can be between 0-255 according to:
-	https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.body_sensor_location.xml
-	 */
-	private void setCharacteristic(int checkedId) {
-		/*
-		done each time the user changes a value of a Characteristic
-		 */
-		int value = checkedId == R.id.rbn_value_1 ? BLEMSG_1 : BLEMSG_2;
-		mSampleCharacteristic.setValue(getValue(value));
-	}
-
-	private byte[] getValue(int value) {
-		return new byte[]{(byte) value};
-	}
-
-	/*
-	send to the client the value of the Characteristic,
-	as the user requested to notify.
-	 */
-	private void notifyCharacteristicChanged() {
-		/*
-		done when the user clicks the notify button in the app.
-		indicate - true for indication (acknowledge) and false for notification (un-acknowledge).
-		 */
-		boolean indicate = (mSampleCharacteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_INDICATE) == BluetoothGattCharacteristic.PROPERTY_INDICATE;
-
-		for (BluetoothDevice device : mBluetoothDevices) {
-			if (mGattServer != null) {
-				mGattServer.notifyCharacteristicChanged(device, mSampleCharacteristic, indicate);
-			}
-		}
-	}
-
-	/**
-	 * Returns Intent addressed to the {@code PeripheralAdvertiseService} class.
-	 */
-	private Intent getServiceIntent(Context context) {
-		return new Intent(context, PeripheralAdvertiseService.class);
-	}
-
 
 	private final BluetoothGattServerCallback mGattServerCallback = new BluetoothGattServerCallback() {
 		/**
@@ -200,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
 		public void onConnectionStateChange(BluetoothDevice device, final int status, int newState) {
 			super.onConnectionStateChange(device, status, newState);
 			TLog.d("status={0} newState={1}", status, newState);
-			TLog.d("status-BluetoothGatt.GATT_SUCCESS({0}) newState-({1})", BluetoothGatt.GATT_SUCCESS, newState);
+			TLog.d("status-BluetoothGatt.GATT_SUCCESS({0}) newState-BluetoothGatt.STATE_xxxx(STATE_CONNECTED({1}),STATE_DISCONNECTED({2}))", BluetoothGatt.GATT_SUCCESS, BluetoothGatt.STATE_CONNECTED, BluetoothGatt.STATE_DISCONNECTED);
 
 			String msg;
 			if (status == BluetoothGatt.GATT_SUCCESS) {
@@ -333,5 +274,7 @@ public class MainActivity extends AppCompatActivity {
 		}
 	};
 
-
+	private byte[] int2bytes(int value) {
+		return new byte[]{(byte) value};
+	}
 }
