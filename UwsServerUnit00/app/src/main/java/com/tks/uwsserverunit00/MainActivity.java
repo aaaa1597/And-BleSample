7a7c7b2a41e21c28f1e73d27f8a270ecb9a3c511
aaaaa <aaaaa>
2021-12-16 16:50:03 +0900

    地図機能を、新規Fragmentに移動。

--- UwsServerUnit00/app/src/main/java/com/tks/uwsserverunit00/MainActivity.java
+++ UwsServerUnit00/app/src/main/java/com/tks/uwsserverunit00/MainActivity.java
@@ -17,54 +17,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.os.Looper;
 import android.os.RemoteException;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
 import com.google.android.material.snackbar.Snackbar;
 import com.tks.uwsserverunit00.ui.DeviceListAdapter;
 
 import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 public class MainActivity extends FragmentActivity {
 //	private FragBizLogicViewModel	mViewModel;
 	private DeviceListAdapter		mDeviceListAdapter;
 	private final static int		REQUEST_PERMISSIONS = 1111;
-	private GoogleMap				mMap;
-	private Location				mLocation;
-	GoogleMap						mGoogleMap;
-	Map<String, SerchInfo>			mSerchInfos = new HashMap<>();
-	int								mNowSerchColor = 0xff78e06b;	/* 緑っぽい色 */
-	/* 検索情報 */
-	static class SerchInfo {
-		public Marker	maker;		/* GoogleMapの Marker */
-		public Polyline	polyline;	/* GoogleMapの polyline */
-	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -134,75 +105,7 @@ public class MainActivity extends FragmentActivity {
 			startForResult.launch(enableBtIntent);
 		}
 
-		/* SupportMapFragmentを取得し、マップを使用する準備ができたら通知を受取る */
-		SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.frgMap);
-		Objects.requireNonNull(mapFragment).getMapAsync(new OnMapReadyCallback() {
-			@Override
-			public void onMapReady(@NonNull GoogleMap googleMap) {
-				TLog.d("mLocation={0} googleMap={1}", mLocation, googleMap);
-				if (mLocation == null) {
-					/* 位置が取れない時は、小城消防署で */
-					mLocation = new Location("");
-					mLocation.setLongitude(130.20307019743947);
-					mLocation.setLatitude(33.25923509336276);
-				}
-				mMap = googleMap;
-				initDraw(mLocation, mMap);
-			}
-		});
-
-		/* 位置情報管理オブジェクト */
-		FusedLocationProviderClient flpc = LocationServices.getFusedLocationProviderClient(this);
-		flpc.getLastLocation().addOnSuccessListener(this, location -> {
-			if (location == null) {
-				TLog.d("mLocation={0} googleMap={1}", location, mMap);
-				LocationRequest locreq = LocationRequest.create().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setInterval(500).setFastestInterval(300);
-				flpc.requestLocationUpdates(locreq, new LocationCallback() {
-					@Override
-					public void onLocationResult(@NonNull LocationResult locationResult) {
-						super.onLocationResult(locationResult);
-						TLog.d("locationResult={0}({1},{2})", locationResult, locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
-						mLocation = locationResult.getLastLocation();
-						flpc.removeLocationUpdates(this);
-						initDraw(mLocation, mMap);
-					}
-				}, Looper.getMainLooper());
-			}
-			else {
-				TLog.d("mLocation=(経度:{0} 緯度:{1}) mMap={1}", location.getLatitude(), location.getLongitude(), mMap);
-				mLocation = location;
-				initDraw(mLocation, mMap);
-			}
-		});
-
 		bindBleService();
-	}
-
-	/* 初期地図描画(起動直後は現在地を表示する) */
-	private void initDraw(Location location, GoogleMap googleMap) {
-		if (location == null) return;
-		if (googleMap == null) return;
-
-		mGoogleMap = googleMap;
-		LatLng nowposgps = new LatLng(location.getLatitude(), location.getLongitude());
-		TLog.d("経度:{0} 緯度:{1}", location.getLatitude(), location.getLongitude());
-		TLog.d("拡縮 min:{0} max:{1}", googleMap.getMinZoomLevel(), googleMap.getMaxZoomLevel());
-
-		/* 現在地マーカ追加 */
-		Marker basemarker = googleMap.addMarker(new MarkerOptions().position(nowposgps).title("BasePos"));
-		mSerchInfos.put("base", new SerchInfo(){{maker=basemarker; polyline=null;}});
-
-		/* 現在地マーカを中心に */
-		googleMap.moveCamera(CameraUpdateFactory.newLatLng(nowposgps));
-		TLog.d("CameraPosition:{0}", googleMap.getCameraPosition().toString());
-
-		/* 地図拡大率設定 */
-		TLog.d("拡縮 zoom:{0}", 19);
-		googleMap.moveCamera(CameraUpdateFactory.zoomTo(19));
-
-		/* 地図俯角 50° */
-		CameraPosition tilt = new CameraPosition.Builder(googleMap.getCameraPosition()).tilt(70).build();
-		googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(tilt));
 	}
 
 	@Override
