c99abe4956cc7d87255832fb126a55c2bd80fe07
aaaaa <aaaaa>
2022-01-27 13:10:52 +0900

    不要プロジェクト削除

--- UwsClient/app/src/main/java/com/tks/uwsclient/ui/FragMainViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.tks.uwsclient.ui;
-
-import androidx.lifecycle.ViewModel;
-
-public class FragMainViewModel extends ViewModel {
-	// TODO: Implement the ViewModel
-}
--- UwsServerUnit00/app/src/main/java/com/tks/uwsserverunit00/ui/FragBizLogicViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.tks.uwsserverunit00.ui;
-
-import androidx.lifecycle.ViewModel;
-
-public class FragBizLogicViewModel extends ViewModel {
-}
